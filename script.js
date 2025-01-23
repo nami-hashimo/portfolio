cd9442d2972762be298bdc53e301029b01939457
nami-hashimo <namicoco0708@gmail.com>
2025-01-23 15:39:46 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".header-nav-sp");
+const navLinks = document.querySelectorAll(".header-nav-sp li");
+
+burger.addEventListener("click", () => {
+
+  nav.classList.toggle("nav-active");
+
+  navLinks.forEach((link, index) => {
+
+    if (link.style.animation) {
+      link.style.animation = "";
+    } else {
+
+      link.style.animation = `navLinksFade 0.5s ease forwards ${index / 7 + 0.4
+        }s`;
+    }
+  });
+
+  burger.classList.toggle("toggle");
+});
+
+navLinks.forEach((link, index) => {
+  link.addEventListener("click", () => {
+    nav.classList.remove("nav-active")
+  })
+});
